1676ba5bd2b920f3c9c38a57d1e70252a5088020
rafakimanja <rafaellopes.ifrs@gmail.com>
2025-10-03 17:56:26 -0300

    estilizando o login form

--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -2,23 +2,23 @@ import Logo from "@/components/logo"
 
 export function LoginForm() {
   return (
-    <form>
-      <div>
+    <form className="h-[600px] w-[450px] bg-gray-400">
+      <div className="h-1/3 flex items-center justify-center">
         <Logo /> 
       </div>
-      <div>
-        <div>
-          <label htmlFor="username">Usuario</label>
-          <input type="text" name="username" id="username" />
+      <div className="h-1/3 flex flex-col justify-center items-center gap-3">
+        <div className="w-80 flex flex-col gap-2">
+          <label htmlFor="username" className="ml-4 font-bold text-xl">Usuario:</label>
+          <input type="text" name="username" id="username" className="bg-gray-500 rounded-2xl h-9 text-white pl-5" />
         </div>
-        <div>
-          <label htmlFor="password">Senha</label>
-          <input type="password" name="password" id="password" />
+        <div className="w-80 flex flex-col gap-2">
+          <label htmlFor="password" className="ml-4 font-bold text-xl" >Senha:</label>
+          <input type="password" name="password" id="password" className="bg-gray-500 rounded-2xl h-9 text-white pl-5" />
         </div>
       </div>
-      <div>
-        <button type="submit">Entrar</button>
-        <button>Cadastre-se</button>
+      <div className=" h-1/3 flex flex-col items-center justify-center gap-2">
+        <button type="submit" className="h-10 w-[200px] rounded-3xl hover:cursor-pointer bg-gray-300">Entrar</button>
+        <button className="h-10 w-[200px] rounded-3xl hover:cursor-pointer bg-gray-300">Cadastre-se</button>
       </div>
     </form>
   )
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/forms/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="flex items-center justify-center h-screen">
         <LoginForm />
     </div>
   )
